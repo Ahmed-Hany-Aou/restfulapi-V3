bb71c9d5927d0aca71824cd10fa328e5d8915328
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-09-14 15:23:34 +0300

    catching exception when removing related resources

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,6 +8,8 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -77,6 +79,21 @@ class Handler extends ExceptionHandler
         if($exception instanceof NotFoundHttpException){
             return $this -> errorResponse("The Specified URL Cannot be found",404);
         }
+
+        if($exception instanceof HttpException){
+            return $this -> errorResponse($exception->getMessage(),$exception->getStatusCode());
+        }
+
+        if($exception instanceof QueryException){
+           $errorCode=$exception ->errorInfo[1];
+           if($errorCode==1451){
+            return $this -> errorResponse("Cannot remove this resource permanetly. it is related with any other resource",409);
+            
+           }
+        }
+
+
+
 
         return parent::render($request, $exception);
     }
