f3c72724862de30b3ba770f528155fce53c1410f
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-09-15 22:41:29 +0300

    implemnted store method for seller product controller

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\Seller;
 
-use App\Http\Controllers\ApiController;
+use App\User;
 use App\Seller;
+use App\Product;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
+use Illuminate\Support\Facades\Storage;
+use App\Transformers\ProductTransformer;
+use Illuminate\Auth\Access\AuthorizationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class SellerProductController extends ApiController
 {
@@ -20,5 +25,25 @@ class SellerProductController extends ApiController
         return $this->showAll($products);
     }
 
-   
+    public function store(Request $request, User $seller) /// we used User because when we add first product to some one he is not  yes a seller (still dindt came a seller)
+    {
+        $rules = [
+            'name' => 'required',
+            'description' => 'required',
+            'quantity' => 'required|integer|min:1',
+           // 'image' => 'required|image', // failed to upload image via post man so i comment this line 
+        ];
+
+        $this->validate($request, $rules);
+
+        $data = $request->all();
+
+        $data['status'] = Product::UNAVAILABLE_PRODUCT;
+        $data['image'] = '1.jpg';
+        $data['seller_id'] = $seller->id;
+
+        $product = Product::create($data);
+
+        return $this->showOne($product);
+    }
 }
