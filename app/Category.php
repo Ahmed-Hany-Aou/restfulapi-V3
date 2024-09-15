9fde930a68092edfdff56809759b0543f352584a
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-09-15 20:33:21 +0300

    removed piovt table from results

--- app/Category.php
+++ app/Category.php
@@ -8,11 +8,17 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Category extends Model
 {
     use SoftDeletes;
+    
     protected $dates =['deleted_at'];
     protected $fillable = [
         'name',
         'description',
     ];
+
+    protected $hidden =[
+        'pivot'
+    ];
+
     public function products()
     {
         return $this->belongsToMany(Product::class);
