ac05e11c6468f285b3842fdae52f020d57f7a6b4
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-09-18 21:12:03 +0300

    handeled avability status using events

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Product;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,16 +15,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-    }
+        Schema::defaultStringLength(191);
+
+        Product::updated(function ($product) {
+            if ($product->quantity == 0 && $product->isAvailable()) {
+                $product->status = Product::UNAVAILABLE_PRODUCT;
+
+                $product->save();
+            }
+    });
 
     /**
      * Register any application services.
      *
      * @return void
      */
-    public function register()
-    {
-        //
-    }
+   
+}
 }
